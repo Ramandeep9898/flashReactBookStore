b12f5e77f9b24754f4a9b654050ab22b905ecc23
Ramandeep9898 <ramank6238299@gmail.com>
2022-06-12 12:57:14 +0530

    detail page functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import react from "react";
 import Header from "./../components/Header.component/Header";
-import Hero from "../components/HomePage.component/Home.Component/Hero.Component/Hero";
+import Hero from "../components/HomePage.component/Home.Component/hero.component/Hero";
 import Categories from "../components/HomePage.component/Home.Component/Categories.Component/Category";
 import Footer from "../components/HomePage.component/Home.Component/Footer.Component/Footer";
 
