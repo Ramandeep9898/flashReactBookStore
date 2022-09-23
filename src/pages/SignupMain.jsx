5a34b9550af5da0d066510107b82cf9ae461b8ff
Ramandeep <ramank6238299@gmail.com>
2022-09-23 18:08:04 +0530

    signup file name

--- src/pages/SignupMain.jsx
+++ src/pages/SignupMain.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SignUp from "../components/Signup.Component/SignUp";
+import SignUp from "../components/Signup.Component/Signup";
 import Header from "../components/Header.component/Header";
 
 const SignupMain = () => {
